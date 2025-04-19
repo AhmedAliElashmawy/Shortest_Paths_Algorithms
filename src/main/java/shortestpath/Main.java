9bc17b735271efb7745857f3491afe4678e97562
AhmedAliElashmawy <trophie.hunter.gamer@gmail.com>
2025-04-19 22:49:10 +0200

    Final

--- src/main/java/shortestpath/Main.java
+++ src/main/java/shortestpath/Main.java
@@ -4,15 +4,6 @@ import shortestpath.utils.ShortestPathStatistics;
 
 public class Main {
     public static void main(String[] args){
-
-
-        // Runs the statistics for the shortest path algorithms
-        ShortestPathStatistics stats = new ShortestPathStatistics();
-
-        stats.runStatisticsForOneSourcedShortestPath();
-        stats.runStatisticsForAllPairsShortestPath();
-
-        
         CLI cli = new CLI();
         cli.mainMenu();
     }
